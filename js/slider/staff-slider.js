358cb706d2042a4a41fc903b57cc69ee9814b21e
roses-5117 <rosesbp1915@gmail.com>
2025-12-24 11:21:42 +0900

    staffスライダー調整

--- js/slider/staff-slider.js
+++ js/slider/staff-slider.js
@@ -1,7 +1,27 @@
 export const initializeAboutStaffSlider = () => {
     // const aboutStaffSlider = document.querySelector(".js-about-staff-slider");
-    new Splide( '.splide' ).mount();
+    // new Splide( '.splide' ).mount();
+    const aboutStaffSlider = document.querySelector(".js-about-staff-slider");
 
-    const splide = new Splide(".splide");
+    if (!aboutStaffSlider) return;
+    
+    const splide = new Splide(aboutStaffSlider,{
+        type: "loop",
+        perPage: 3,
+        perMove: 1,
+        arrows: false,
+        pagination: false,
+        autoplay: true,
+        interval: 3000,
+        gap: "40rem",
+        padding:{left:"172rem",right:"259rem"},
+        breakpoints:{
+            899:{
+                perPage: 1,
+                padding:{left:"20rem",right:"125rem"},
+                gap: "32rem",
+            },
+        },
+    });
     splide.mount();
 };
